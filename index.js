86ed694ac96c82f4a95ff0923f396f1f46918c00
masinc <masinc000@gmail.com>
2022-10-03 19:54:10 +0900

    add main.js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-import process from 'node:process'
+import process from 'node:process';
+import child_process from 'node:child_process';
 
-console.log(`current dir: ${process.cwd()}`);
-console.log(process.env);
+function init_action() {
+    const repo = process.env['GITHUB_ACTION_REPOSITORY'];
+    const repoUrl = process.env['GITHUB_SERVER_URL'] + '/' + repo;
+    const dir = path.join('/tmp/', repo);
+    const branch = 'main';
+
+    child_process.execSync(`git clone --depth 1 --single-branch -b ${branch} ${repoUrl} ${dir}`);
+
+    process.chdir(dir);
+
+    child_process.execSync('npm ci');
+    child_process.execSync('npm start');
+}
+
+init_action()
---
+++ main.js
@@ -0,0 +1,6 @@
+import core from "@actions/github";
+import github from "@actions/github";
+
+console.log(core.context);
+console.log();
+console.log(github.context);
