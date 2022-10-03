004b016970f75f54df56bf3a436f93999946b6f8
masinc <masinc000@gmail.com>
2022-10-03 21:12:00 +0900

    add random value

--- main.mjs
+++ main.mjs
@@ -1,4 +1,5 @@
 import 'zx/globals';
 cd($.env['GITHUB_WORKSPACE']);
 
+console.log('::set-output name=rand::' + Math.random());
 console.log((await $`cat package.json`).stdout);
--- main.ps1
+++ main.ps1
@@ -1,2 +1,4 @@
 Set-Location $env:GITHUB_WORKSPACE
 Get-Content ./package.json
+
+Write-Output "::set-output name=rand::$(Get-Random)"
